00ab89f566d5a66b92a1df5f8c41c7764bf30d2d
Kirill Ivashov <ivashov@cs.karelia.ru>
2014-04-04 17:08:57 +0400

    Buttons for files

--- AudioGuide/src/main/java/org/fruct/oss/audioguide/fragments/edit/EditPointDialog.java
+++ AudioGuide/src/main/java/org/fruct/oss/audioguide/fragments/edit/EditPointDialog.java
@@ -1,12 +1,15 @@
 package org.fruct.oss.audioguide.fragments.edit;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import org.fruct.oss.audioguide.R;
@@ -14,7 +17,8 @@ import org.fruct.oss.audioguide.track.Point;
 import org.fruct.oss.audioguide.util.Utils;
 
 public class EditPointDialog extends DialogFragment implements DialogInterface.OnClickListener {
-	private Listener listener;
+    private static final int IMAGE_REQUEST_CODE = 0;
+    private Listener listener;
 
 	public interface Listener {
 		void pointCreated(Point point);
@@ -27,6 +31,9 @@ public class EditPointDialog extends DialogFragment implements DialogInterface.O
 	private EditText editName;
 	private EditText editDescription;
 	private EditText editUrl;
+
+    private EditText imageFileText;
+    private Button imageFileButton;
 
 	public EditPointDialog(Point point) {
 		if (point == null) {
@@ -45,13 +52,23 @@ public class EditPointDialog extends DialogFragment implements DialogInterface.O
 
 		editName = (EditText) view.findViewById(R.id.text_title);
 		editDescription = (EditText) view.findViewById(R.id.text_description);
-		//editUrl = (EditText) view.findViewById(R.id.edit_url);
+
+        imageFileText = (EditText) view.findViewById(R.id.image_file);
+        imageFileButton = (Button) view.findViewById(R.id.image_file_button);
+        //editUrl = (EditText) view.findViewById(R.id.edit_url);
 
 		if (point != null) {
 			if (!Utils.isNullOrEmpty(point.getName())) editName.setText(point.getName());
 			if (!Utils.isNullOrEmpty(point.getDescription())) editDescription.setText(point.getDescription());
 			//if (!Utils.isNullOrEmpty(point.point())) editUrl.setText(point.getUrl());
 		}
+
+        imageFileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFileChooserDialog();
+            }
+        });
 
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		builder.setView(view)
@@ -83,7 +100,26 @@ public class EditPointDialog extends DialogFragment implements DialogInterface.O
 		}
 	}
 
+    private void showFileChooserDialog() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        startActivityForResult(Intent.createChooser(intent, "Choose file"), IMAGE_REQUEST_CODE);
+    }
+
 	public void setListener(Listener listener) {
 		this.listener = listener;
 	}
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IMAGE_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                imageFileText.setText(data.getDataString());
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
