a155466353df39222fddd3526fc07decefb1d64a
Kirill Ivashov <ivashov@cs.karelia.ru>
2014-06-04 18:11:28 +0400

    Exponential slider preference

--- AudioGuide/src/main/java/org/fruct/oss/audioguide/preferences/SliderPreference.java
+++ AudioGuide/src/main/java/org/fruct/oss/audioguide/preferences/SliderPreference.java
@@ -17,7 +17,33 @@ import org.fruct.oss.audioguide.R;
 import org.fruct.oss.audioguide.util.AUtils;
 
 public class SliderPreference extends DialogPreference implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
+	private static class Converter {
+		int toSliderPosition(int value) {
+			return value;
+		}
+
+		int toValue(int sliderPosition) {
+			return sliderPosition;
+		}
+	}
+
+	private static class ExponentialConverter extends Converter {
+		private double ln10 = Math.log(10);
+
+		@Override
+		int toSliderPosition(int value) {
+			return (int) (1000 * Math.log(value - 500) / ln10);
+		}
+
+		@Override
+		int toValue(int sliderPosition) {
+			return (int) (Math.exp(sliderPosition* ln10 / 1000) + 500);
+		}
+	}
+
 	private static final String NS="http://schemas.android.com/apk/res/android";
+
+	private final Converter converter;
 
 	private final int defaultValue;
 	private final int maxValue;
@@ -29,6 +55,8 @@ public class SliderPreference extends DialogPreference implements SeekBar.OnSeek
 	private String textStr;
 	private int textRes;
 
+	private boolean isExponential;
+
 	public SliderPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
 
@@ -38,6 +66,12 @@ public class SliderPreference extends DialogPreference implements SeekBar.OnSeek
 		textRes = attrs.getAttributeResourceValue(NS, "text", -1);
 		if (textRes == -1)
 			textStr = attrs.getAttributeValue(NS, "text");
+
+		isExponential = attrs.getAttributeBooleanValue(null, "exponential", false);
+		if (isExponential)
+			converter = new ExponentialConverter();
+		else
+			converter = new Converter();
 	}
 
 	@Override
@@ -62,8 +96,9 @@ public class SliderPreference extends DialogPreference implements SeekBar.OnSeek
 	@Override
 	protected void onBindDialogView(View v) {
 		super.onBindDialogView(v);
-		seekBar.setMax(maxValue);
-		seekBar.setProgress(value);
+
+		seekBar.setMax(converter.toSliderPosition(maxValue));
+		seekBar.setProgress(converter.toSliderPosition(value));
 	}
 
 	@Override
@@ -78,8 +113,10 @@ public class SliderPreference extends DialogPreference implements SeekBar.OnSeek
 	}
 
 	@Override
-	public void onProgressChanged(SeekBar seekBar, int i, boolean fromUser) {
-		textView.setText(getContext().getResources().getQuantityString(textRes, i, i));
+	public void onProgressChanged(SeekBar seekBar, int progressBarPosition, boolean fromUser) {
+		int value = converter.toValue(progressBarPosition);
+
+		textView.setText(getContext().getResources().getQuantityString(textRes, value, value));
 	}
 
 	@Override
@@ -103,9 +140,11 @@ public class SliderPreference extends DialogPreference implements SeekBar.OnSeek
 	@Override
 	public void onClick(View view) {
 		if (shouldPersist()) {
-			value = seekBar.getProgress();
-			persistInt(seekBar.getProgress());
-			callChangeListener(Integer.valueOf(seekBar.getProgress()));
+			int progress = seekBar.getProgress();
+
+			value = converter.toValue(progress);
+			persistInt(value);
+			callChangeListener(value);
 		}
 
 		getDialog().dismiss();
